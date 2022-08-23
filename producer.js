50b0719478520260f89f3b5503ebb88e29dfafe7
Jundee <jundeemark.molina@carsu.edu.ph>
2022-08-23 14:25:21 +0800

    Added Documentations

--- producer.js
+++ producer.js
@@ -1,15 +1,22 @@
 // producer.js
 
-var filename = "example/input.txt";
+/* Importing the file system module. */
 var fs = require('fs');
-var zmq = require("zeromq"),
-  sock = zmq.socket("push");
+/* Importing the zeromq module. */
+var zmq = require("zeromq");
 
+/* Defining the file that will be read. */
+var filename = "example/input.txt";
+
+/* Creating a socket of type `push`. */
+sock = zmq.socket("push");
+
+/* Binding the socket to the port 3000. */
 sock.bindSync("tcp://127.0.0.1:3000");
 console.log("Producer bound to port 3000");
 
+/* Sending the file content to the consumer every 500 milliseconds. */
 setInterval(function() {
-  // console.log("sending work");
   const fileContent = fs.readFileSync("example/input.txt", 'utf8');
   sock.send(fileContent);
 }, 500);
--- worker.js
+++ worker.js
@@ -1,13 +1,18 @@
 // worker.js
 
+/* Loading the zeromq library. */
 var zmq = require("zeromq");
+/* Loading the file system module. */
 var fs = require('fs');
-  sock = zmq.socket("pull");
 
+/* Creating a socket of type `pull`. */
+sock = zmq.socket("pull");
+
+/* Connecting to the port 3000. */
 sock.connect("tcp://127.0.0.1:3000");
 console.log("Worker connected to port 3000");
 
+/* Listening for a message from the socket. When it receives a message, it writes it to a file. */
 sock.on("message", function(msg) {
-  // console.log("work: %s", msg.toString());
   fs.writeFileSync("example/output.txt", msg);
 });
